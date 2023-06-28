dc972ff7698cc1711712a122f6a287bbe1d1b78a
hifoomin <jakubrataj@protonmail.com>
2023-06-28 14:15:52 +0200

    j 2

--- AcridTweaks/ConfigManager.cs
+++ AcridTweaks/ConfigManager.cs
@@ -11,6 +11,7 @@ namespace HIFUAcridTweaks
     {
         internal static bool ConfigChanged = false;
         internal static bool VersionChanged = false;
+        public static MethodInfo method;
 
         public static void HandleConfigAttributes(Type type, string name, string description, string section, object defaultValue, ConfigFile config)
         {
@@ -25,34 +26,35 @@ namespace HIFUAcridTweaks
                 Debug.LogErrorFormat("data: {0}, {1}, {2}, {3}, {4}, {5}", type, name, description, section, defaultValue, config);
                 Main.HACTLogger.LogError("field is " + field.Name + " type: " + t);
 
-                MethodInfo method = typeof(ConfigFile).GetMethods().Where(x => x.Name == nameof(ConfigFile.Bind)).First();
+                method = typeof(ConfigFile).GetMethods().Where(x => x.Name == nameof(ConfigFile.Bind)).First();
 
-                Main.HACTLogger.LogError("methodinfo is " + method.Name);
+                Debug.LogErrorFormat("methodinfo: {0}, {1}", method, method.Name);
 
                 method = method.MakeGenericMethod(t);
 
-                Main.HACTLogger.LogError("generic method is " + method.Name);
+                Debug.LogErrorFormat("generic method: {0}, {1}", method, method.Name);
 
-                /*
                 ConfigEntryBase val = (ConfigEntryBase)method.Invoke(config, new object[] { new ConfigDefinition(section, name), defaultValue, new ConfigDescription(description) });
                 ConfigEntryBase backupVal = (ConfigEntryBase)method.Invoke(Main.HACTBackupConfig, new object[] { new ConfigDefinition(Regex.Replace(config.ConfigFilePath, "\\W", "") + " : " + section, name), val.DefaultValue, new ConfigDescription(description) });
                 // problems here -- [Error  : Unity Log] TargetException: Non-static method requires a target.
-
-                Main.HACTLogger.LogError(section + " : " + name + " " + val.DefaultValue + " / " + val.BoxedValue + " ... " + backupVal.DefaultValue + " / " + backupVal.BoxedValue + " >> " + VersionChanged);
-
-                if (!ConfigEqual(backupVal.DefaultValue, backupVal.BoxedValue))
+                Debug.LogErrorFormat("val and backupVal: {0} {1}", val, backupVal);
+                if (val != null && backupVal != null)
                 {
-                    Main.HACTLogger.LogError("Config Updated: " + section + " : " + name + " from " + val.BoxedValue + " to " + val.DefaultValue);
-                    if (VersionChanged)
+                    Main.HACTLogger.LogError(section + " : " + name + " " + val.DefaultValue + " / " + val.BoxedValue + " ... " + backupVal.DefaultValue + " / " + backupVal.BoxedValue + " >> " + VersionChanged);
+
+                    if (!ConfigEqual(backupVal.DefaultValue, backupVal.BoxedValue))
                     {
-                        Main.HACTLogger.LogError("Autosyncing...");
-                        val.BoxedValue = val.DefaultValue;
-                        backupVal.BoxedValue = backupVal.DefaultValue;
+                        Main.HACTLogger.LogError("Config Updated: " + section + " : " + name + " from " + val.BoxedValue + " to " + val.DefaultValue);
+                        if (VersionChanged)
+                        {
+                            Main.HACTLogger.LogError("Autosyncing...");
+                            val.BoxedValue = val.DefaultValue;
+                            backupVal.BoxedValue = backupVal.DefaultValue;
+                        }
                     }
+                    if (!ConfigEqual(val.DefaultValue, val.BoxedValue)) ConfigChanged = true;
+                    field.SetValue(null, val.BoxedValue);
                 }
-                if (!ConfigEqual(val.DefaultValue, val.BoxedValue)) ConfigChanged = true;
-                field.SetValue(null, val.BoxedValue);
-                */
             }
         }
 
